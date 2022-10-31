6b38c7400d9b856c8a84522ff19dded88b2e41d3
CodingSergey <a.b.games.business@gmail.com>
2022-10-31 18:51:01 +0100

    Vercel collection manager

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import TopBar from "../components/TopBar";
 import { ThemeProvider } from "@mui/material";
 import {theme} from "../styles/Theme";
 export default function Profile() {
-    const [collections, setCollections] = useState();
+    const [collections, setCollections] = useState([]);
     const fetchCollections = async () => {
         const response = await fetch("https://vast-garden-06972.herokuapp.com/collection/usercollections", {
             method: "POST",
@@ -16,7 +16,6 @@ export default function Profile() {
             body: JSON.stringify({_author: localStorage.getItem("username")})
         })
         const res = await response.json();
-        console.log(res);
         setCollections(res);
     }
     useEffect(()=>{
@@ -26,7 +25,14 @@ export default function Profile() {
         <ThemeProvider theme={theme}>
         <Container justifycontent="center">
             <TopBar/>
-            
+            {collections.map(collection => {
+                <div>
+                   <Typography>{collection.name}</Typography>
+                   <Typography>{collection.description}</Typography>
+                   <Typography>{collection.topic}</Typography>
+                   <Typography>{collection.date}</Typography>
+                </div>
+            })}
         </Container>
         </ThemeProvider>
     )
