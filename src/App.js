3cb9a40bfa4849b0e5453d5bfca569ae80d43f61
CodingSergey <a.b.games.business@gmail.com>
2022-11-01 14:15:30 +0100

    Project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Dashboard from "./pages/Dashboard";
 import Login from "./authentication/Login";
 import Register from "./authentication/Register"
@@ -7,23 +7,23 @@ import { ThemeProvider } from "@emotion/react";
 import { createTheme } from "@mui/system";
 import Profile from "./pages/Profile";
 import Admin from "./pages/Admin";
-const theme= createTheme({
-  mode:"light"
-})
+import { theme } from "./styles/Theme";
+import { TopBar } from "./components/TopBar";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Dashboard/>} />
-        <Route exact path="/login" element={<Login/>} />
-        <Route exact path="/register" element={<Register/>} />
-        <Route exact path="/addCollection" element={<AddCollection/>}/>
-        <Route exact path="/profile" element={<Profile/>}/>
-        <Route exact path="/manager" element={<Admin/>}/>
-      </Routes>
-    </BrowserRouter>
+      <TopBar />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/addCollection" element={<AddCollection />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/manager" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
